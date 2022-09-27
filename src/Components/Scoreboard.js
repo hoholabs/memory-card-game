ce70806ee81e49ff697dc8d210c359f9f01c08db
hoholabs <johnpaulhoran@gmail.com>
2022-09-27 19:06:03 -0400

    High Score added

--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ScoreBoard(props) {
+    // const { score } = this.props;
+
+    const [highScore, setHighScore] = useState('0');
+
+    useEffect(() => {
+        if (highScore < props.score) {
+            setHighScore(props.score);
+        }
+    }, [highScore, props.score]);
+
     return (
         <div id="scoreBoard">
             <div>Score:{props.score}</div>
+            <div>High Score:{highScore}</div>
         </div>
     );
 }
